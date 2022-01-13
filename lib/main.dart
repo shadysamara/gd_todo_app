8ad0d2c775908aedab49df01d231f4bea0377ba5
shady samara <shadysamara@shadys-MacBook-Pro.local>
2022-01-13 14:03:03 +0200

    localizatoin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,67 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_ui/ui/todo_main_page.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: TodoMainPage(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('ar')],
+        path: 'assets/langs', // <-- change the path of the translation files
+        fallbackLocale: Locale('en'),
+        child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: TodoMainPage(),
+    );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.language),
+        onPressed: () {
+          if (context.locale == Locale('en')) {
+            context.setLocale(Locale('ar'));
+          } else {
+            context.setLocale(Locale('en'));
+          }
+        },
+      ),
+      appBar: AppBar(
+        title: Text('app_name'.tr()),
+      ),
+      body: Center(
+        child: Text('course_name'.tr()),
+      ),
+    );
+  }
+}
+
+class ResponsiveTestScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        height: MediaQuery.of(context).size.height / 4,
+        width: MediaQuery.of(context).size.width / 2,
+        color: Colors.red,
+      ),
+    );
+  }
 }
--- lib/ui/todo_main_page.dart
+++ lib/ui/todo_main_page.dart
@@ -47,83 +47,133 @@ class _TodoMainPageState extends State<TodoMainPage>
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.done),
-          onPressed: () {
-            tabController.animateTo(1);
-          },
-        ),
-        drawer: Drawer(
-          child: Column(
-            children: [
-              UserAccountsDrawerHeader(
-                  currentAccountPicture: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    child: Text('O'),
+    return MediaQuery.of(context).orientation == Orientation.portrait
+        ? Scaffold(
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.done),
+              onPressed: () {
+                tabController.animateTo(1);
+              },
+            ),
+            drawer: Drawer(
+              child: Column(
+                children: [
+                  UserAccountsDrawerHeader(
+                      currentAccountPicture: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: Text('O'),
+                      ),
+                      accountName: Text('Omar'),
+                      accountEmail: Text('omar@gmail.com')),
+                  ListTile(
+                    onTap: () {
+                      tabController.animateTo(0);
+                    },
+                    title: Text('All Tasks'),
+                    trailing: Icon(Icons.arrow_forward_ios),
                   ),
-                  accountName: Text('Omar'),
-                  accountEmail: Text('omar@gmail.com')),
-              ListTile(
-                onTap: () {
-                  tabController.animateTo(0);
-                },
-                title: Text('All Tasks'),
-                trailing: Icon(Icons.arrow_forward_ios),
+                  ListTile(
+                    onTap: () {
+                      tabController.animateTo(1);
+                    },
+                    title: Text('Complete Tasks'),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      tabController.animateTo(2);
+                    },
+                    title: Text('InComplete Tasks'),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                  ),
+                ],
               ),
-              ListTile(
-                onTap: () {
-                  tabController.animateTo(1);
-                },
-                title: Text('Complete Tasks'),
-                trailing: Icon(Icons.arrow_forward_ios),
+            ),
+            appBar: AppBar(
+              title: Text('TODO APP'),
+              bottom: TabBar(
+                controller: tabController,
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.list),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.done),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.cancel_outlined),
+                  ),
+                ],
               ),
-              ListTile(
-                onTap: () {
-                  tabController.animateTo(2);
-                },
-                title: Text('InComplete Tasks'),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          title: Text('TODO APP'),
-          bottom: TabBar(
-            controller: tabController,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.list),
-              ),
-              Tab(
-                icon: Icon(Icons.done),
-              ),
-              Tab(
-                icon: Icon(Icons.cancel_outlined),
-              ),
-            ],
-          ),
-        ),
-        // bottomNavigationBar: BottomNavigationBar(
-        //   currentIndex: index,
-        //   onTap: (newindex) {
-        //     setState(() {
-        //       this.index = newindex;
-        //     });
-        //   },
-        //   items: [
-        //     BottomNavigationBarItem(label: 'All', icon: Icon(Icons.list)),
-        //     BottomNavigationBarItem(label: 'Complete', icon: Icon(Icons.done)),
-        //     BottomNavigationBarItem(
-        //         label: 'InComplete', icon: Icon(Icons.cancel)),
-        //   ],
-        // ),
-        body: TabBarView(controller: tabController, children: [
-          AllTasksScreen(updateAllScreens, deleteTask),
-          CompleteTasksScreen(updateAllScreens, deleteTask),
-          InCompleteTasksScreen(updateAllScreens, deleteTask),
-        ]));
+            ),
+            // bottomNavigationBar: BottomNavigationBar(
+            //   currentIndex: index,
+            //   onTap: (newindex) {
+            //     setState(() {
+            //       this.index = newindex;
+            //     });
+            //   },
+            //   items: [
+            //     BottomNavigationBarItem(label: 'All', icon: Icon(Icons.list)),
+            //     BottomNavigationBarItem(label: 'Complete', icon: Icon(Icons.done)),
+            //     BottomNavigationBarItem(
+            //         label: 'InComplete', icon: Icon(Icons.cancel)),
+            //   ],
+            // ),
+            body: TabBarView(controller: tabController, children: [
+              AllTasksScreen(updateAllScreens, deleteTask),
+              CompleteTasksScreen(updateAllScreens, deleteTask),
+              InCompleteTasksScreen(updateAllScreens, deleteTask),
+            ]))
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('TODO APP'),
+            ),
+            body: Row(
+              children: [
+                Expanded(
+                  child: ListView(
+                    children: [
+                      UserAccountsDrawerHeader(
+                          currentAccountPicture: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            child: Text('O'),
+                          ),
+                          accountName: Text('Omar'),
+                          accountEmail: Text('omar@gmail.com')),
+                      ListTile(
+                        onTap: () {
+                          tabController.animateTo(0);
+                        },
+                        title: Text('All Tasks'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      ListTile(
+                        onTap: () {
+                          tabController.animateTo(1);
+                        },
+                        title: Text('Complete Tasks'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                      ListTile(
+                        onTap: () {
+                          tabController.animateTo(2);
+                        },
+                        title: Text('InComplete Tasks'),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                    child: TabBarView(controller: tabController, children: [
+                  AllTasksScreen(updateAllScreens, deleteTask),
+                  CompleteTasksScreen(updateAllScreens, deleteTask),
+                  InCompleteTasksScreen(updateAllScreens, deleteTask),
+                ]))
+              ],
+            ),
+          );
   }
 }
 
